9d55abf5786ba1a2c85f81fec5259beb2b18d775
Ka-lip <22840544+Ka-lip@users.noreply.github.com>
2026-03-23 17:19:03 +0900

    feat: add UI and validation to distributePercentage.js
    Enhance distributePercentage.js with user interface prompts and input
    validation. Now checks if the selection is a SysML part, alerts if no
    ports are found, and asks for user confirmation before proceeding.
    Adds completion alert and copyright/license header.

--- executable_scripts/distributePercentage.js
+++ executable_scripts/distributePercentage.js
@@ -1,4 +1,22 @@
+//$EXPERIMENTAL$  $ENHANCED_CONTAINMENT_ACCESS$
+/*
+ * Copyright 2017-2026 ANSYS, Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+ * the Software.
+ * v2026-03-23 - KL initial version
+ */
+load(".lib/ui.js");
 function main(){
+  if (selection[0] instanceof Metamodel.sysml.SysMLPart == false){
+    alert("Please select a SysML part.\n Aborted.");
+    return;
+  }
   var ports = [];
   var subElements = selection[0].the_owned_elements.toArray();
   for (var i = 0; i < subElements.length; i++){
@@ -7,12 +25,19 @@ function main(){
     }
   }
 
+  if (ports.length == 0) {
+    alert("No ports are found.\nAborted.");
+    return;
+  }
   var percent = (java.math.BigDecimal("100").divide(java.math.BigDecimal(ports.length.toString()) , 18, java.math.BigDecimal.ROUND_HALF_UP));
-
+  var confirm_msg = ("Found " + ports.length + " ports, the ratio of each port will be " + 100/ports.length +"%. Do you want to proceed?");
+  var user_confirm = alertWithAbortOption("CONFIRMATION", confirm_msg);
+  if (user_confirm) { return;}
   for (var i = 0; i < ports.length; i++){
     ports[i].percentage = percent;
     ports[i].failureRateMode = "FROM_PARENT";
   }
+  alert("Process completed. If the result is not as expected, please press CTRL + Z to undo it.");
 }
 
 main();
